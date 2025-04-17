ee2e3aa17e0c89a9159f4fafc7553205d7a17e4e
ldanie38 <ldanie38@asu.edu>
2025-04-16 21:25:47 -0400

    Describe your changes here

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,request,session,redirect
 from flask_app import app
-#from flask_app.models.user import User
+from flask_app.models.user_model import User
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
 from datetime import datetime
