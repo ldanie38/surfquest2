2c9c5876c2c0d059ff8e510f766f5eb66c6b5162
ldanie38 <ldanie38@asu.edu>
2025-04-16 21:10:49 -0400

    Describe your changes here

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -7,3 +7,58 @@ from datetime import datetime
 from flask import flash
 import os
 from pprint import pprint
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    
+    return render_template('login.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+        if not User.validate_user(request.form):
+            return redirect('/')
+    #pw_hash is a variable
+        pw_hash = bcrypt.generate_password_hash(request.form['password'])
+        print(pw_hash)
+        data = {
+        "username": request.form["username"],
+        "email":request.form["email"],
+        "password":pw_hash
+    
+    }
+
+
+        user_id=User.save(data)
+        session['user_id'] = user_id
+        print(user_id)
+        return redirect('/home')
+    
+
+@app.route('/login_user',methods=["POST"])
+def login_user():
+        
+    user_in_db= User.get_by_email(request.form)
+    
+
+    if not user_in_db:
+        flash('Invalid email/password')
+        return redirect('/')
+    
+    if not bcrypt.check_password_hash(user_in_db.password, request.form['password']):
+        flash('Ivalid email/password !!!!')
+        return redirect('/')
+    session['user_id'] = user_in_db.id
+    
+
+    return redirect ('/home')
+    
--- flask_app/modlels/user_model.py
+++ flask_app/modlels/user_model.py
@@ -2,3 +2,54 @@ from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
 import re
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+class User:
+    def __init__( self , data ):
+        self.id = data['id']
+        self.username = data['username']
+        self.email = data['email']
+        self.password=data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        
+
+    @classmethod
+    def save(cls, data):
+        query = ''' INSERT INTO users (username,email,password)
+        VALUES (%(username)s,%(email)s,%(password)s);
+        '''
+        results= connectToMySQL('project').query_db(query,data)
+        return results
+    
+
+
+    @staticmethod
+    def validate_user(data):
+        is_valid = True
+        if len(data['username']) <= 1:
+            flash("Username must be more than one character!")
+            is_valid = False
+        if len(data['email']) == 0:
+            flash("Email is required!")
+            is_valid = False
+        if not EMAIL_REGEX.match(data['email']):
+            flash("Invalid email address!")
+            is_valid = False
+        query = "SELECT * FROM users WHERE email=%(email)s"
+        results = connectToMySQL('project').query_db(query, data)
+        if len(results) != 0:
+            flash("Email is already in use!")
+            is_valid = False
+        if len(data['password']) == 0:
+            flash("Password is required!")
+            is_valid = False
+
+        return is_valid
+    
+    @classmethod
+    def get_by_email(cls,data):
+        query = "SELECT * FROM users WHERE email = %(email)s"
+        results= connectToMySQL('project').query_db(query,data)
+        if len(results) < 1:
+            return False
+        return cls(results[0])
