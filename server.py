014380c15941f7ff1909ad875359d01e70545c63
ldanie38 <ldanie38@asu.edu>
2025-06-10 00:16:37 -0400

    Updating server.py and adding Procfile

--- server.py
+++ server.py
@@ -4,8 +4,12 @@ from flask_app.controllers import comments
 from flask_app.controllers import surf_conditions
 from flask_app.controllers import places
 from flask_app import app 
+import os
 
 
 
-if __name__=="__main__":
-    app.run(debug=True,port=4242)
+
+if __name__ == "__main__":
+    # Heroku sets the PORT dynamically, fallback to 4242 for local dev
+    PORT = int(os.getenv("PORT", 4242))
+    app.run(debug=True, host="0.0.0.0", port=PORT)
